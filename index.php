79031a76021f236d6fb7c9880182a4207a70c704
try46 <popuman61@gmail.com>
2018-01-26 21:28:56 +0900

    no message

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "hello from the php";
+/*
+変数とデータ型
+*/
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+      Hello World <?php echo "from php"; ?>
+    </p>
+  </body>
+</html>
